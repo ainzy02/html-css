d505173c4733c9242735c410eab25e2523e72f54
ainzy02 <113187416+ainzy02@users.noreply.github.com>
2024-05-21 21:09:31 -0400

    2

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function apple(){
+
+function apple() {
+    document.body.innerHTML += "<div id='draggable3' style='background-color: lightskyblue;width:100px;'>dragMe3</div>\n"
     draggable1 = new PlainDraggable(document.getElementById('draggable1'));
     draggable2 = new PlainDraggable(document.getElementById('draggable2'));
+    draggable3 = new PlainDraggable(document.getElementById('draggable3'));
+    createNew();
+}
+var y=5;
+function createNew()
+{
+    document.body.innerHTML += "<div id='draggable" + y + "\'style='background-color: lightskyblue;width:100px;'>dragMe3</div>\n"
+connectDivToElements();
+}
+function connectDivToElements()
+{
+    draggabley = new PlainDraggable(document.getElementById('draggable'+y));
+    draggabley = new PlainDraggable(document.getElementById('draggable2'));
+    draggabley = new PlainDraggable(document.getElementById('draggable1'));
+    draggabley = new PlainDraggable(document.getElementById('draggable3'));
+}
 
-}
-function createNew(){
-    document.getElementById("theScreen").innerHTML +="<div id='draggable1' style="background-color: lightskyblue;width:100px;\">dragMe1</div>\n"
-}
+
+document.getElementById(outputHere).innerHTML=(document.getElementById(draggable5).height);
