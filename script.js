eeab1c530936d2f34651517a7cb0f8b3931b6f2d
ainzy02 <113187416+ainzy02@users.noreply.github.com>
2024-05-30 06:01:27 -0400

    a

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+let y = 0;
 
-let y=0;
 // let counter=0;
 
-function createNew()
-{
+function createNew() {
     document.body.innerHTML += "<div id='draggable" + y + "\'style='background-color: lightskyblue;width:100px;'>Drag Me!</div>\n"
+    document.body.innerHTML += "<div id='testingThis" + y + "\'>locatation!</div>\n";
     connectDivToElements();
     y++;
     alert(y);
 }
-function connectDivToElements()
-{
-    for(var counter = 0; y>=counter; counter++ ) {
+
+
+function connectDivToElements() {
+    for (let counter = 0; y >= counter; counter++) {
         draggable = new PlainDraggable(document.getElementById('draggable' + counter));
+        draggable.onDrag = function () {
+            if (this.rect.left !== null && this.rect.top !== null)
+                document.getElementById('testingThis' + counter).innerHTML = this.rect.left + " " + this.rect.top + " " + this.rect.height + " " + this.rect.width;
+        }
     }
 }
-
